7a69996a1ef7593a0a3dccc3fb8078dd9c7a039a
Sav Walters <savwalts@users.noreply.github.com>
2021-01-25 19:41:23 -0800

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Header.css';
+
+import {
+  storeCurrentUser,
+  clearCurrentUser
+} from '../auth';
 
 import './Header.css';
 
@@ -11,7 +19,7 @@ const Header = ({
 
   useEffect(() => {
     setSelectedUser(userList[0]);
-  }, [userList]);
+  }, [ userList ]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,11 +32,13 @@ const Header = ({
   }
 
   const handleUserLogin = (event) => {
+    storeCurrentUser(selectedUser); // NEW
     setCurrentUser(selectedUser);
   }
 
   const handleUserLogout = (event) => {
     setSelectedUser(userList[0]);
+    clearCurrentUser(); // NEW
     setCurrentUser(null);
   }
 
@@ -40,8 +50,12 @@ const Header = ({
         onSubmit={ handleSubmit } >
         {
           currentUser
-            ? <button onClick={ handleUserLogout }>LOG OUT, { currentUser.username }</button>
-            : <>
+          ? <> 
+              <NavLink to="/posts" activeClassName="current">POSTS</NavLink>
+              <NavLink to="/todos" activeClassName="current">TODOS</NavLink >
+              <button onClick={ handleUserLogout }>LOG OUT, { currentUser.username }</button>
+            </>
+          : <>
               <select onChange={ handleSelectChange }>{
                 userList.map(user => (
                   <option key={ user.id } value={ user.id }>
